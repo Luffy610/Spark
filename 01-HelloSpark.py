5a796ca0416cd81a8184b1f7d29edb7b2e418dd4
Luffy610 <dhruvkotwani@outlook.com>
2024-09-03 00:12:15 +0530

    1st Session

---
+++ 01-HelloSpark.py
@@ -0,0 +1,12 @@
+from pyspark.sql import *
+
+spark = SparkSession.builder \
+    .appName("Hello Spark") \
+    .master('local[2]') \
+    .getOrCreate()
+
+data_list = [('Ravi', 28), ('David', 45), ('Abdul', 37)]
+
+df = spark.createDataFrame(data_list).toDF("Name", "Age")
+
+df.show()
